83544165b8fa7b40b66af60f180e32711db48c2f
Mehedi Hasan Monirul <bdmehedi92@gmail.com>
2017-07-03 18:46:04 +0600

    Prepare to upload version 1.0 with update database

--- phonebook.sql
+++ phonebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2017 at 03:18 PM
+-- Generation Time: Jul 03, 2017 at 02:44 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -37,8 +37,11 @@ CREATE TABLE `categories` (
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'IT Company', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `categories` (`id`, `category_name`, `created_at`, `updated_at`) VALUES
+(1, 'IT Company', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'IGL client', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'SMS Client', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Business', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -48,7 +51,7 @@ INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
 
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -57,7 +60,7 @@ CREATE TABLE `groups` (
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `name`, `created_at`, `updated_at`) VALUES
+INSERT INTO `groups` (`id`, `group_name`, `created_at`, `updated_at`) VALUES
 (1, 'admin', '2017-07-01 18:01:00', '0000-00-00 00:00:00'),
 (2, 'user', '2017-07-01 18:04:00', NULL);
 
@@ -75,6 +78,22 @@ CREATE TABLE `numbers` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `numbers`
+--
+
+INSERT INTO `numbers` (`id`, `number`, `category_id`, `added_by`, `created_at`, `updated_at`) VALUES
+(69, '01686023434', 2, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(70, '01686023435', 1, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(71, '01686023436', 2, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(72, '01686023437', 1, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(73, '01686023438', 2, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(74, '01686023439', 2, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(75, '01686023431', 3, 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(76, '01683023432', 3, 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(77, '01823037728', 3, 10, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(80, '01823037727', 3, 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -99,7 +118,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_name`, `mobile`, `name`, `password`, `group_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(4, 'mehedi', '01767248797', 'Mehedi Hasan', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(4, 'mehedi', '01767248797', 'Mehedi Hasan', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'monirul', '01686023434', 'Monirul Islam', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'm', '01823037726', '123', '2ac9a6746aca543af8dff39894cfe8173afba21eb01c6fae33d52947222855ef', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(9, 'mm', '01767248798', 'mm man', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(10, 'mmm', '01823037727', 'mmm', '2ac9a6746aca543af8dff39894cfe8173afba21eb01c6fae33d52947222855ef', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -122,6 +145,7 @@ ALTER TABLE `groups`
 --
 ALTER TABLE `numbers`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `number` (`number`),
   ADD KEY `category_id` (`category_id`);
 
 --
@@ -140,7 +164,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `groups`
 --
@@ -150,12 +174,12 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `numbers`
 --
 ALTER TABLE `numbers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Constraints for dumped tables
 --
